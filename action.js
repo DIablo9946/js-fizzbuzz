2d461d95e7462dde8f99b0eacd691ef4200c8e67
Diablo9946 <diabloch9946@gmail.com>
2019-07-04 16:31:29 +0200

    Stuct

--- action.js
+++ action.js
@@ -1,3 +1,29 @@
-document.getElementById('my')
+document.getElementById('my');
 
-console.log("Hi there")
+console.log("Hi there");
+
+
+
+for (var i = 0; i < 100; i++) {
+  console.log('i');
+}
+
+if (i / 3 % 0) {
+  console.log[i];
+  document.writeln('Fizz');
+}
+
+else if (i / 5 % 0) {
+  console.log[i];
+  document.writeln('Buzz');
+}
+
+else if (i / 3 % 0 && i / 5 % 0) {
+  console.log[i];
+  document.writeln('FizzBuzz');
+}
+
+else {
+  console.log[i];
+  document.writeln(i)
+}
