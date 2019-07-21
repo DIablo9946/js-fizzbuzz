5f974435fc1891b2dad2706d29a67c3efd3dbc9a
Diablo9946 <diabloch9946@gmail.com>
2019-07-21 16:30:58 +0200

    It prints the conditions with the numbers.

--- action.js
+++ action.js
@@ -1,29 +1,33 @@
-document.getElementById('my');
+// Fizz Buzz test: Scrivi un programma che stampi i numeri da 1 a 100
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli
+//  di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// Esattamente quelle devono essere le stringhe di output.
+// *NOTE=* fate output in console -
+// poi se vi sentite di andar oltre fate l’output in una ul,
+//  come abbiamo visto nuovamente stamattina
+//  (ovviamente nell’ul van sempre dei <li>) su altra versione
+//  (quindi altra sottocartella).
 
-console.log("Hi there");
+// Mi stabilisco le variabili
 
+var stampa;
 
+stampa = document.getElementById('my');
 
-for (var i = 0; i < 100; i++) {
-  console.log('i');
+// Faccio il ciclo for
+
+for (i = 0; i < 101; i++) {
+  console.log(i);
+  // Faccio le causali
+  if (i % 3 == 0) {
+    console.log("Fizz");
+  } if (i % 5 == 0) {
+    console.log("Buzz");
+  } else if (i % 5 & i % 3 == 0) {
+    console.log("FizzBuzz");
+  }
 }
 
-if (i / 3 % 0) {
-  console.log[i];
-  document.writeln('Fizz');
-}
 
-else if (i / 5 % 0) {
-  console.log[i];
-  document.writeln('Buzz');
-}
-
-else if (i / 3 % 0 && i / 5 % 0) {
-  console.log[i];
-  document.writeln('FizzBuzz');
-}
-
-else {
-  console.log[i];
-  document.writeln(i)
-}
+// Faccio la stampa in ul
